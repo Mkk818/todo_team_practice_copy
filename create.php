e074d25da3c8da7668f84f9fc2d2a5d657b5b8c7
Atsuko Inoue <sinba.805i@gmail.com>
2021-04-17 19:40:25 +0700

    Create 1st commit

--- create.php
+++ create.php
@@ -6,10 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Todoアプリ</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="./style.css"> -->
 </head>
 <body>
+    <div class="header">
+        <nav class="headerbar">        
+         <a href="index.php">Todoアプリ</a>
+        </nav>    
+    </div>
+        <form action="store.php" method="post">
 
+            <div class="title">  
+                <label for>Title</label>
+                <input type="text" name="title" id="title" size="100">
+        
+             </div>
 
+            <div class="contents">
+                <label for="contents">Contents</label>
+                <textarea name="contents" id="contents" rows="10" cols="100"></textarea>
+            </div>
+
+            <!-- <div class="file">
+                <input type="file"name="file">
+            </div> -->
+    
+            <input type="button" value="送信">
+
+        </form>
 </body>
 </html>
--- store.php
+++ store.php
@@ -1,1 +1,15 @@
 <?php
+// ファイルの読み込み
+require_once('Models/Model.php');
+
+// データの受け取り(スーパーグローバル変数)
+$title=$_POST['title'];
+$contents=$_POST['contents'];
+
+// Taskクラスの呼び出し、実行
+$task = new Task();
+$task -> create([$title,$contents]);
+
+// リダイレクト
+header('location:index.php');
+exit;
