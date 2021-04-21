eee34ba8692ca3dca17fd219076304d86b37352b
ArisaNAKAMURA <arisanakamura.eng@gmail.com>
2021-04-21 12:21:21 +0900

    body部分の追加
    ## edit.php
    井上さんの開発したcreate.phpをコピーしてbody部分に追加しました。
    POST送信先をupdate.phpに変更しました。

--- edit.php
+++ edit.php
@@ -18,7 +18,31 @@ $task = $task->findById($id);
     <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-   
+<div class="header">
+        <nav class="headerbar">        
+         <a href="index.php">Todoアプリ</a>
+        </nav>    
+    </div>
+    <div class="form">
+        <form action="update.php" method="post">
+
+            <div class="title">  
+                <label for="title">Title</label>
+                <input type="text" name="title" id="title">
+        
+             </div>
+
+            <div class="contents">
+                <label for="contents">Contents</label>
+                <textarea name="contents" id="contents" rows="10" cols="100"></textarea>
+            </div>
+            <input type="hidden" name="id" value="<?= h($task['id']); ?>">
+            <div class="submit-button">
+             <button type="submit">更新</button>
+            </div>
+       
+        </form>
+    </div>
    
 </body>
 </html>
